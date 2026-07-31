5e5e56464a4783c96bc7d31413b6ed1f9d10052f
Daniel Lerma <danjeager12@gmail.com>
2026-07-31 15:24:41 -0500

    config6

--- courses/nodejs-course-midu/class-04-rest-api-deploy/middlewares/cors.js
+++ courses/nodejs-course-midu/class-04-rest-api-deploy/middlewares/cors.js
@@ -3,8 +3,8 @@ import cors from "cors";
 const ACCEPTED_ORIGINS = [
   "http://127.0.0.1:5500",
   "http://localhost:1234",
-  "https://movies.com",
-  "https://midu.dev",
+  "https://web-development-zvwi.onrender.com",
+  "https://rest-api-deploy-front.onrender.com",
 ];
 
 export const corsMiddleware = ({ acceptedOrigins = ACCEPTED_ORIGINS } = {}) =>

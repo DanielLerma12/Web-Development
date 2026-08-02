8774a0448669055fa5e6cb703c43480c2443ac15
Daniel Lerma <danjeager12@gmail.com>
2026-08-02 13:04:22 -0500

    chat01

--- courses/nodejs-course-midu/class-05-chat-realtime/client/script.js
+++ courses/nodejs-course-midu/class-05-chat-realtime/client/script.js
@@ -1,6 +1,69 @@
-const socket = io();
+let username = localStorage.getItem("username");
 
-document.addEventListener("click", (e) => {
+if (!username) {
+  const response = await fetch("https://randomuser.me/api/");
+
+  const data = await response.json();
+
+  username = data.results[0].login.username;
+
+  console.log(username);
+
+  localStorage.setItem("username", username);
+}
+
+const socket = io("http://localhost:3000", {
+  auth: {
+    serverOffset: 0,
+    username,
+  },
+});
+
+const form = document.querySelector("#form");
+const input = document.querySelector("#input");
+const messages = document.querySelector("#messages");
+
+socket.on("chat message", (result) => {
+  const date = new Date(result.created_at);
+  const item = `
+      <li>
+        <strong>${result.content}</strong>
+        <div>
+    <p>${result.username}</p>
+    <p>${date.toLocaleTimeString()}</p>
+    </div>
+
+      </li>
+    `;
+
+  messages.insertAdjacentHTML("beforeend", item);
+  socket.auth.serverOffset = result.id;
+  messages.scrollTop = messages.scrollHeight;
+});
+
+socket.on("chat history", (result) => {
+  messages.innerHTML = "";
+  result.forEach((r) => {
+    const date = new Date(r.created_at);
+    const item = `
+      <li>
+        <strong>${r.content}</strong>
+        <div>
+    <p>${r.username}</p>
+    <p>${date.toLocaleTimeString()}</p>
+    </div>
+
+      </li>
+    `;
+
+    messages.insertAdjacentHTML("beforeend", item);
+  });
+});
+
+form.addEventListener("submit", (e) => {
   e.preventDefault();
-  console.log("Holaaaa");
+  if (input.value) {
+    socket.emit("chat message", input.value);
+    input.value = "";
+  }
 });
--- courses/nodejs-course-midu/class-05-chat-realtime/server/index.js
+++ courses/nodejs-course-midu/class-05-chat-realtime/server/index.js
@@ -3,25 +3,79 @@ import pico from "picocolors";
 import logger from "morgan";
 import { Server } from "socket.io";
 import { createServer } from "node:http";
+import cors from "cors";
+
+import { prisma } from "../lib/prisma.js";
+
+await prisma.$connect();
+console.log(pico.magenta("connected to Postgres supabase"));
 
 const app = express(); // express crea todo por uno, pero solo crea el servidor al momento de hacer listen, ahora esa lógica la maneja el server de http
 const server = createServer(app); // se crea un servidor que usa a app
-const io = new Server(server); // socket.io se conecta al server
+const io = new Server(server, {
+  cors: {
+    origin: "http://127.0.0.1:5500",
+    methods: ["GET", "POST"],
+  },
+  connectionStateRecovery: {
+    maxDisconnectionDuration: 2 * 60 * 1000,
+    skipMiddlewares: true,
+  },
+}); // socket.io se conecta al server
 // tanto app como socket.io usan el mismo server
 
-io.on("connection", (socket) => {
-  console.log("a user has connected!");
+io.on("connection", async (socket) => {
+  console.log("a user has connected");
+
+  socket.on("chat message", async (msg) => {
+    let result;
+    const username = socket.handshake.auth.username;
+
+    try {
+      result = await prisma.messages.create({
+        data: {
+          content: msg,
+          username,
+        },
+      });
+      io.emit("chat message", result);
+    } catch (e) {
+      console.log(e);
+      return;
+    }
+  });
+
+  if (!socket.recovered) {
+    try {
+      const result = await prisma.messages.findMany({
+        where: {
+          id: {
+            gt: socket.handshake.auth.serverOffset ?? 0, // revisar esto
+          },
+        },
+        orderBy: {
+          id: "asc",
+        },
+      });
+
+      if (result !== undefined) {
+        io.emit("chat history", result);
+      }
+    } catch (e) {
+      console.log(e);
+      return;
+    }
+  }
+
   socket.on("disconnect", () => {
-    console.log("an user has disconnected");
+    console.log("a user has disconnected");
   });
 });
 
 app.use(logger("dev"));
 
-app.use(express.static("client"));
-
 app.get("/", (req, res) => {
-  res.sendFile(process.cwd() + "/client/index.html");
+  res.send("Hola");
 });
 
 const PORT = process.env.PORT ?? 3000;

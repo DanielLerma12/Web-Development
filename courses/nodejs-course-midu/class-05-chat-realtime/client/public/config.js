bb66229514a8025ce5b437b979489a875ca51119
Daniel Lerma <danjeager12@gmail.com>
2026-08-06 12:37:45 -0500

    db3

--- courses/nodejs-course-midu/class-05-chat-realtime/client/public/config.js
+++ courses/nodejs-course-midu/class-05-chat-realtime/client/public/config.js
@@ -1,2 +1,2 @@
-/* export const API_URL = "https://chat-socketio-jtuc.onrender.com"; */
-export const API_URL = "http://localhost:3000";
+export const API_URL = "https://chat-socketio-jtuc.onrender.com";
+/* export const API_URL = "http://localhost:3000"; */
